60a1ba8df4a2fe81ab7ab4a0301687d73ab2acbf
janguni <lightson23@naver.com>
2022-08-25 23:01:26 +0900

    pricipal 객체로 memberEmail 확인

--- src/main/java/com/petchatbot/controller/PetController.java
+++ src/main/java/com/petchatbot/controller/PetController.java
@@ -2,6 +2,7 @@ package com.petchatbot.controller;
 
 import com.petchatbot.config.ResponseMessage;
 import com.petchatbot.config.StatusCode;
+import com.petchatbot.config.auth.PrincipalDetails;
 import com.petchatbot.domain.dto.MemberDto;
 import com.petchatbot.domain.requestAndResponse.ChangePetInfoReq;
 import com.petchatbot.domain.requestAndResponse.DefaultRes;
@@ -14,9 +15,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @Slf4j
 @RestController
@@ -27,18 +33,27 @@ public class PetController {
     private final MemberService memberService;
     private final PetService petService;
 
+    // 반려동물 추가
     @PostMapping("/addPet")
-    public ResponseEntity<String> addPet(@RequestBody PetRegReq petRegReq) {
-
+    public ResponseEntity<String> addPet(@RequestBody PetRegReq petRegReq, Authentication authentication) {
+        PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
+        log.info("member->{}", principal.getMember().getMemberEmail());
         petService.registerPet(petRegReq);
         log.info("petRegReg={}",petRegReq.getPetName());
         return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.ADD_PET), HttpStatus.OK);
     }
 
+    // 반려동물 프로필 변경
     @PostMapping("/changePetInfo")
     public ResponseEntity<String> addPet(@RequestBody ChangePetInfoReq petInfoReq) {
         petService.changePetInfo(petInfoReq);
         log.info("petRegReg={}",petInfoReq.getPetName());
         return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.SUCCESS_CHANGE_PET_INFO), HttpStatus.OK);
     }
+
+
+    // 반려동물 목록
+//    @GetMapping("/")
+
+
 }
