8ee7f02f42798b398a51faa0d1a7ff4a713a56ea
janguni <lightson23@naver.com>
2022-08-31 20:18:00 +0900

    질병 관련 파일 주석처리

--- src/main/java/com/petchatbot/repository/DiseasesRepository.java
+++ src/main/java/com/petchatbot/repository/DiseasesRepository.java
@@ -1,10 +1,6 @@
 package com.petchatbot.repository;
 
-import com.petchatbot.domain.model.Diseases;
-import org.bson.types.ObjectId;
-import org.springframework.data.domain.Example;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
